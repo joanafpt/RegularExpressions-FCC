b58aa863662269b31c6418a91b69708a818ffd61
Joana <joanajteixeira@outlook.com>
2019-02-15 19:00:36 +0000

    //Regular Expressions: Reuse Patterns Using Capture Groups
    //Use capture groups in reRegex to match numbers that are repeated only three times in a string, each separated by a space.

---
+++ reusePatternsUsingCaptureGroups.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Reuse Patterns Using Capture Groups
+
+//Use capture groups in reRegex to match numbers that are repeated only three times in a string, each separated by a space.
+
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
+let result = reRegex.test(repeatNum);
+
+// \1 refers to the stuff that must be repeated (\d+).
