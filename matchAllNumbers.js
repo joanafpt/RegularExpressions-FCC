5359ffabdf3975247221c3fa1be55a4fd582dcca
Joana <joanajteixeira@outlook.com>
2019-02-15 18:56:45 +0000

    //Regular Expressions: Match All Numbers
    //Use the shorthand character class \d to count how many digits are in movie titles. Written out numbers ("six" instead of 6) do not count.

---
+++ matchAllNumbers.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Match All Numbers
+
+//Use the shorthand character class \d to count how many digits are in movie titles. Written out numbers ("six" instead of 6) do not count.
+
+let numString = "Your sandwich will be $5.00";
+let numRegex = /[\d]/ig; // Change this line
+let result = numString.match(numRegex).length;
+
+console.log(numString.match(numRegex).length);
