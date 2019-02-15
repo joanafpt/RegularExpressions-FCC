796fbe49a051d2236a2410cef92df139f094b016
Joana <joanajteixeira@outlook.com>
2019-02-15 18:57:37 +0000

    //Regular Expressions: Match Characters that Occur Zero or More Times
    /*Create a regex chewieRegex that uses the * character to match all the upper and lower"a" characters in chewieQuote. Your regex does not need flags, and it should not match any of the other quotes.*/

---
+++ matchCharactersThatOccurZeroOrMoreTimes.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Match Characters that Occur Zero or More Times
+
+/*Create a regex chewieRegex that uses the * character to match all the upper and lower"a" characters in chewieQuote. Your regex does not need flags, and it should not match any of the other quotes.*/
+
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/; // Change this line
+let result = chewieQuote.match(chewieRegex);
+
+console.log(chewieQuote.match(chewieRegex));
