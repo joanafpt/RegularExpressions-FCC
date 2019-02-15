2d5bac8ba85aa52c191c15dac281171206598219
Joana <joanajteixeira@outlook.com>
2019-02-15 18:34:25 +0000

    //Regular Expressions: Check for All or None
    /* Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.*/

---
+++ checkForAllOrNone.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Check for All or None
+/* Change the regex favRegex to match both the American English (favorite) and the British English (favourite) version of the word.*/
+
+
+let favWord = "favorite";
+let favRegex = /favou?rite/i; // Change this line
+let result = favRegex.test(favWord);
+
+console.log(favRegex.test(favWord));
