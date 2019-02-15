6fce5637ca2a8838b4c9502374d16eee6e6166ca
Joana <joanajteixeira@outlook.com>
2019-02-15 18:57:50 +0000

    //Regular Expressions: Match Ending String Patterns
    //Use the anchor character ($) to match the string "caboose" at the end of the string caboose.

---
+++ matchEndingStringPatterns.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Match Ending String Patterns
+//Use the anchor character ($) to match the string "caboose" at the end of the string caboose.
+
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let result = lastRegex.test(caboose);
+
+console.log(lastRegex.test(caboose));
+//returns true
