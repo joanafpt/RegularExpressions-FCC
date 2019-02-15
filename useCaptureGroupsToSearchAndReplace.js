40ac4c2680cf132b6fe422fd50675427ceb6692f
Joana <joanajteixeira@outlook.com>
2019-02-15 19:01:43 +0000

    //Regular Expressions: Use Capture Groups to Search and Replace
    //Write a regex so that it will search for the string "good". Then update the replaceText variable to replace "good" with "okey-dokey".

---
+++ useCaptureGroupsToSearchAndReplace.js
@@ -0,0 +1,10 @@
+//Regular Expressions: Use Capture Groups to Search and Replace
+
+//Write a regex so that it will search for the string "good". Then update the replaceText variable to replace "good" with "okey-dokey".
+
+let huhText = "This sandwich is good.";
+let fixRegex = /good/; // Change this line
+let replaceText = "okey-dokey"; // Change this line
+let result = huhText.replace(fixRegex, replaceText);
+
+console.log(huhText.replace(fixRegex, replaceText));
