ea6a7596a35033944981a81b81dbee1b358b3f3b
Joana <joanajteixeira@outlook.com>
2019-02-15 19:00:58 +0000

    //Regular Expressions: Specify Upper and Lower Number of Matches
    //Change the regex ohRegex to match only 3 to 6 letter h's in the word "Oh no".

---
+++ specifyOnlyLowerNumberOfMatches.js
@@ -0,0 +1,7 @@
+//Regular Expressions: Specify Only the Lower Number of Matches
+//Change the regex haRegex to match the word "Hazzah" only when it has four or more letter z's.
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // Change this line
+let result = haRegex.test(haStr);
+console.log(haRegex.test(haStr));
