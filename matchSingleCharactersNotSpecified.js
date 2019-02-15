07e315f542ab80771a1c943be1059a20c1293df2
Joana <joanajteixeira@outlook.com>
2019-02-15 18:59:17 +0000

    //Regular Expressions: Match Single Characters Not Specified
    //Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.

---
+++ matchSingleCharactersNotSpecified.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Match Single Characters Not Specified
+
+//Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex.
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/ig; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+console.log(quoteSample.match(myRegex));
