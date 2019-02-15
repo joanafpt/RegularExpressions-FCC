f332be7e7d396814284cedc6b3d1799fb955c500
Joana <joanajteixeira@outlook.com>
2019-02-15 18:58:12 +0000

    //Regular Expressions: Match Letters of the Alphabet
    /*Match all the letters in the string quoteSample.
    
    Note
    Be sure to match both upper- and lowercase letters.*/

---
+++ matchLettersOfTheAlphabet.js
@@ -0,0 +1,13 @@
+//Regular Expressions: Match Letters of the Alphabet
+
+/*Match all the letters in the string quoteSample.
+
+Note
+Be sure to match both upper- and lowercase letters.*/
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+
+console.log(quoteSample.match(alphabetRegex));
+
