f197cb5881b8de43ac0c94984d06f5e331a6093e
Joana <joanajteixeira@outlook.com>
2019-02-15 18:58:01 +0000

    //Regular Expressions: Match Everything But Letters and Numbers
    //Use the shorthand character class \W to count the number of non-alphanumeric characters in various quotes and strings.

---
+++ matchEverythingButLettersAndNumbers.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Match Everything But Letters and Numbers
+
+//Use the shorthand character class \W to count the number of non-alphanumeric characters in various quotes and strings.
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /[\W]/ig; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
+
+console.log((nonAlphabetRegex).length);
