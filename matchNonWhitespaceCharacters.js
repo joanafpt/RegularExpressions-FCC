8e7296d4cb5c125a1d2305e3647d7740c74c98eb
Joana <joanajteixeira@outlook.com>
2019-02-15 18:58:57 +0000

    //Regular Expressions: Match Non-Whitespace Characters
    //Change the regex countNonWhiteSpace to look for multiple non-whitespace characters in a string.

---
+++ matchNonWhitespaceCharacters.js
@@ -0,0 +1,9 @@
+//Regular Expressions: Match Non-Whitespace Characters
+
+//Change the regex countNonWhiteSpace to look for multiple non-whitespace characters in a string.
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
+
+console.log(sample.match(countNonWhiteSpace));
